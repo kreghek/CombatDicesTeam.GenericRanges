b0d702168d9798760e5b10e530e859a5792ffb8f
Pavel Kurkutov <pkurkutov@ptsecurity.com>
2024-03-25 16:15:52 +0700

    Add ranges

---
+++ src/CombatDicesTeam.GenericRanges/GenericRange.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace CombatDicesTeam.GenericRanges;
+
+public sealed record GenericRange<T>(T Min, T Max)
+{
+    [PublicAPI]
+    public static GenericRange<T> CreateMono(T value)
+    {
+        return new GenericRange<T>(value, value);
+    }
+}
---
+++ src/CombatDicesTeam.GenericRanges/README.md
@@ -0,0 +1,3 @@
+# CombatDicesTeam.GenericRanges
+
+The library to work with range of values for indie game devs.
